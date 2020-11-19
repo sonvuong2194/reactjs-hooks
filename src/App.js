44940754245777055f105132b103fac857ad427b
Son Vuong <sonvuong2194@gmail.com>
2020-11-19 10:48:50 +0700

    Day la reactjs hooks co ban

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.scss";
+import ColorBox from "./components/ColorBox";
+
+function App() {
+  return (
+    <div className="App">
+      <ColorBox />
+    </div>
+  );
+}
+
+export default App;
